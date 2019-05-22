f0d66fe3b31d2f7353cdfc462e0c173bf45721c3
Christy Inman <christyinman@ymail.com>
2019-05-22 01:47:23 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,8 +7,8 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  num = [1, 2, 3]
-  num.push(4)
+  array = ["wow", "I", "am", "really", "learning"]
+  array << "arrays!"
 end
 
 def add_element_to_start_of_array(array, element)
