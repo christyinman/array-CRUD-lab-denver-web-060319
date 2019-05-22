509977d81ad2aa7d3aedf4808b5012abfd8e9ede
Christy Inman <christyinman@ymail.com>
2019-05-22 01:44:42 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,8 +7,8 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  array = [1, 2, 3]
-  array << 4
+  num = [1, 2, 3]
+  num.push(4)
 end
 
 def add_element_to_start_of_array(array, element)
